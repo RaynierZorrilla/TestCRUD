feb974da38457ebc81d1a6a4957bd6cb5749a42c
Raynier Zorrilla <Raynier Zorrilla@LAPTOP-UFFK3NJM>
2023-07-25 23:14:15 -0400

    Agregar archivos de proyecto.

---
+++ TestCRUD/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TestCRUD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=LAPTOP-UFFK3NJM;Initial Catalog=MyTestDB;Integrated Security=True;Pooling=False");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("insert into UserTab values (@ID, @Name, @Age)", conn);
+            cmd.Parameters.AddWithValue("@ID", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@Name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@Age", double.Parse(textBox3.Text));
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            MessageBox.Show("Saved");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=LAPTOP-UFFK3NJM;Initial Catalog=MyTestDB;Integrated Security=True;Pooling=False");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("Update UserTab set Name=@Name, Age=@Age Where ID=@ID",conn);
+            cmd.Parameters.AddWithValue("@ID", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@Name", textBox2.Text);
+            cmd.Parameters.AddWithValue("@Age", double.Parse(textBox3.Text));
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            MessageBox.Show("Update");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=LAPTOP-UFFK3NJM;Initial Catalog=MyTestDB;Integrated Security=True;Pooling=False");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("Delete UserTab Where ID=@ID", conn);
+            cmd.Parameters.AddWithValue("@ID", int.Parse(textBox1.Text));
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Delete");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=LAPTOP-UFFK3NJM;Initial Catalog=MyTestDB;Integrated Security=True;Pooling=False");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("Select * from UserTab", conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
